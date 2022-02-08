b5a3c7592fc2ec0bee0da3d0023bd71caa64a139
kishan <kishan_97@hotmail.co.uk>
2022-02-08 11:08:28 +0000

    my first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    int myInt = 2;
+    double myDouble = myInt;
+    string myRed = "Red Paint & ";
+    double red = 13.99;
+    string myBlue = "Blue Paint ";
+    double blue = 15.79;
+    double Name = red + blue; 
+      Console.WriteLine("The price of " + myRed + myBlue + "is £" + Name);    
+      Console.WriteLine(myInt);
+      Console.WriteLine(myDouble);
+    }
+  }
+}
